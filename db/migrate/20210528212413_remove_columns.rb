23a2c2a8bfbec5f103921156aa8e51b9df958260
juxsalley <juxsalley@gmail.com>
2021-06-02 10:58:43 +0000

    update schema

--- db/migrate/20210528212413_remove_columns.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class RemoveColumns < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :users, :favorites
-    add_column :users, :favorites, :json, array: true
-    # Ex:- add_column("admin_users", "username", :string, :limit =>25, :after => "email")
-  end
-end
